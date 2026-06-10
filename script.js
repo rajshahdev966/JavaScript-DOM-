851e587c301705a16225d44aa200b067e5de63df
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-06-10 07:29:08 +0530

    Update project files

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-let prompt = require('prompt-sync')();
-let random = Math.floor((Math.random()*100)+1);
-let input;
-do {
-  input = Number(prompt("Guess the number between 1-100 "));
-  if(input > random) {console.log("OH NO! Input is more than random")}
-  else if (input === random) {console.log("Congrats you guessed it!");}
-  else {console.log("Oh NO! Input is less than random")};
-}while(input !== random);
+function isStrong(n){
+  let factorial = 1;
+  let copy = n
+  while(n>0){
+    let rem = n%10
+    for(let i = 1; i<= rem; i++){
+      
+    }
+  }
+}
