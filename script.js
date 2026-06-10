c9fbf3bedbbb651da6d10b1b07cd8d4b839a9c11
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-06-10 07:32:53 +0530

    Update project files

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 function isStrong(n){
   let factorial = 1;
+  let sumfactorial = 0;
   let copy = n
   while(n>0){
-    let rem = n%10
+    let rem = n%10;
     for(let i = 1; i<= rem; i++){
-      
-    }
-  }
+      factorial = factorial*1; 
+      sumfactorial = sumfactorial + factorial;
+    };
+    n = Math.floor(n/10);
+  };
+  if (sumfactorial === copy) console.log("Strong")
+  else console.log("Weak");
 }
