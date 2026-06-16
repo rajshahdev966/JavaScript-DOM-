d32df542dc219170265bd2528aa24f7a88e40026
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-06-16 15:16:40 +0530

    Auto sync changes

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-let prompt = require('prompt-sync')()
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-let k = Number(prompt("Left Rotoation by how many times? "));
-k = k % arr.length;
-for (let j = 1; j <= k; j++) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    let temp = arr[i + 1];
-    arr[i + 1] = arr[i];
-    arr[i] = temp;
+function parent() {
+  console.log("I an Parent...");
+  function child() {
+    console.log("I am child");
+    return 50;
   }
+
 }
-console.log(arr);
+
+parent()
