31d1fc9763674cd3bc916825a5c764af02eed6d0
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-06-16 07:06:49 +0530

    Update project files

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-// let prompt = require('prompt-sync')();
-let num = Number(prompt("Enter the Number"))    ;
-let tablearr = [];
-for(let i = 1; i<tablearr.length; i++){
-    tablearr.push(i);
+let prompt = require('prompt-sync')();
+let n = prompt("Number")
+let count = 10;
+let isbnsum = 0;
+while(n>0){
+    let rem = n%10;
+    isbnsum = (rem*count) + isbnsum
+    count--
+    n = Math.floor(n/10)
 }
-console.table(tablearr)
+if(isbnsum % 11 === 0) return true;
+return false;
