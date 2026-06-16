2f10ba35b0a55e1ca158174eb6221dcf438ad4e0
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-06-16 08:51:46 +0530

    Modified website assets

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ let prompt = require("prompt-sync")();
 let arr = [1, 2, 3, 4, 5,  6];
 let maxindex = 0;
 let max = arr[0]
+for(let i = 1; i<arr.length;i++){
+    if(arr[i]>max){
+        max = arr[i];
+        maxindex++;
+    }
+}
+console.log([max, maxindex]);
