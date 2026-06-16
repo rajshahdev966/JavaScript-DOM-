de1d44de213ccc4612ab8e8ea86081e5124e253a
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-06-16 08:03:37 +0530

    Modified website assets

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let prompt = require('prompt-sync')();
-let n = prompt("Enter the number")
+let prompt = require("prompt-sync")();
+let n = prompt("Enter the number");
 
-for(let  i = 1; i<=n;i++){
-    for(let j = 1;j<=i;j++){
-        process.stdout.write(j + " ")
-    }
-    console.log()
+for (let i = 1; i <= n; i++) {
+  for (let j = n - i + 1; j >= 1; j--) {
+    process.stdout.write("* ");
+  }
+  console.log();
 }
