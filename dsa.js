be05a68d2670385e109fde80dd87f154b575ace8
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-06-16 23:44:23 +0530

    Modified website assets

--- dsa.js
+++ dsa.js
@@ -1,9 +1,9 @@
-let arr = [10, 20, 69, 5, 12, 68, 48]
-let T = 70
+let arr = [10, 20, 69, 5, 12, 68, 48];
+let T = 70;
 let index = -1;
-for(let i = 0; i<arr.length; i++){
-    if(arr[i] === T){
-        index = i;
-    }
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] === T) {
+    index = i;
+  }
 }
 index == -1 ? console.log("Not Found") : console.log(index);
