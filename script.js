66257d4b678c4e5be99a62c8671d9aab83526ba0
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-06-16 11:42:08 +0530

    Update project files

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let arr = [1,2,3,4,5,6,7,8,9];
-for(let i = 0; i<arr.length-1; i++){
-    let temp = arr[i+1];
-    arr[i+1] = arr[i]
+for(let i = arr.length -1; i>0; i--){
+    let temp = arr[i-1];
+    arr[i-1] = arr[i]
     arr[i] = temp;
 }
 console.log(arr);
