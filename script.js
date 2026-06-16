32c8abe877bd248290a5140c698d18e0ffb5f0ec
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-06-16 09:22:09 +0530

    Modified website assets

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let arr = [1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1];
+let arr = [-5, 6, 7, -8, -11, -50, 50, 60, 40, -25, 1, -1, 0];
 for(let i = 0, j = 0; i<arr.length && j<arr.length;i++){
-    if(arr[i] === 0){
+    if(arr[i] < 0){
         let temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
