5ffc1025c55fe7c42bac245b7d04fa1ffecab90a
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-06-16 07:08:27 +0530

    Auto sync changes

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let prompt = require('prompt-sync')();
-let n = prompt("Number")
+let n = prompt("Number");
 let count = 10;
 let isbnsum = 0;
 while(n>0){
     let rem = n%10;
-    isbnsum = (rem*count) + isbnsum
-    count--
-    n = Math.floor(n/10)
+    isbnsum = (rem*count) + isbnsum;
+    count--;
+    n = Math.floor(n/10);
 }
-if(isbnsum % 11 === 0) return true;
-return false;
+if(isbnsum % 11 === 0) console.log(true)
+else console.log(false);
