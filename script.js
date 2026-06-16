7705548a8b9ec3ea0faac6b977901c3d6f110430
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-06-16 11:48:34 +0530

    Updated portfolio content

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-let arr = [1,2,3,4,5,6,7,8,9];
-for(let i = arr.length -1; i>0; i--){
-    let temp = arr[i-1];
-    arr[i-1] = arr[i]
+let prompt = require('prompt-sync')()
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let k = Number(prompt("Left Rotoation by how many times? "));
+k = k % arr.length;
+for (let j = 1; j <= k; j++) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let temp = arr[i + 1];
+    arr[i + 1] = arr[i];
     arr[i] = temp;
+  }
 }
 console.log(arr);
