30d59b2beed820f05c2080e3a15aebaf53074c1c
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-06-16 07:21:59 +0530

    Updated portfolio content

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-let prompt = require('prompt-sync')();
-let n  = Number(prompt("Number "))
+let prompt = require("prompt-sync")();
+let n = Number(prompt("Number "));
 let copy = n;
-let nsq = n**2;
-let rem;
-let count = 0;
-while(n>0){
-    let rem = n%10;
-    count++;
-    n = Math.floor(n/10);
+let sumdigi = 0;
+while (n > 0) {
+  let rem = n % 10;
+  sumdigi = sumdigi + rem;
+  n = Math.floor(n / 10);
 }
-if(nsq %  (10**(count)) === copy) console.log(true)
+if (copy % sumdigi === 0) console.log(true);
 else console.log(false);
