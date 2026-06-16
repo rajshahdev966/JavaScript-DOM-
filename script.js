8412554fbc1dc7da6c7abecd8e7c14db4111b75e
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-06-16 07:16:57 +0530

    Auto sync changes

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 let prompt = require('prompt-sync')();
-let n = prompt("Number");
+let n  = prompt("Number ")
+let copy = n;
 let nsq = n**2;
-if(String(nsq).endsWith(n)) console.log(true)
+let rem;
+let count = 0;
+while(n>0){
+    let rem = n%10;
+    count++;
+    n = n%10;
+}
+if(nsq %  (10**(count)) === copy) console.log(true)
 else console.log(false);
