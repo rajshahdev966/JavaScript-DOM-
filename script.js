36e09770f89617c68dbd6b2ebdf492c79ec34e3a
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-06-16 08:23:37 +0530

    Update project files

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 let prompt = require("prompt-sync")();
-let arr = [ ]
+let arr = [1, 2, 3, 4, 5,  6];
+let sum = 0;
+for(let  i = 0; i< arr.length ; i++){
+    sum = arr[i] + sum;
+
+}
+console.log(sum);
