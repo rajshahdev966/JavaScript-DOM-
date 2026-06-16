e968e42f358fb91383b94894b87e7b103b501acb
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-06-16 07:38:13 +0530

    Update project files

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 let prompt = require("prompt-sync")();
 let n = Number(prompt("Number "));
 let copy = n;
-let copy2 = n;
-let count = 0;
-while (n > 0) {
-  count++;
-  n = Math.floor(n/10);
+let sumfact = 0
+
+while(n>0){
+    let rem = n%10;
+    let fact = 1;
+    for(let i = 1; i<=n; i++){
+        fact = fact * i
+    }
+    sumfact = sumfact + fact;
+    n = Math.floor(n/10)
 }
-let sumpow = 0;
-while (copy > 0) {
-  let rem = copy % 10;
-  sumpow = rem ** count + sumpow;
-  copy = Math.floor(copy/10);
-}
-if (sumpow === copy2) console.log(true);
+if(sumfact === copy) console.log(true)
 else console.log(false);
+
+
