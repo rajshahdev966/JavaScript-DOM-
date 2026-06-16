c1638a87157690f588bfd777bfd9f15f50ab8710
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-06-16 08:59:17 +0530

    Update project files

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 let prompt = require("prompt-sync")();
 let arr = [1, 2, 3, 4, 5,  6];
-let maxindex = 0;
 let max = arr[0]
 for(let i = 1; i<arr.length;i++){
     if(arr[i]>max){
         max = arr[i];
-        maxindex = i;
     }
 }
 let maxremovearr = arr.filter((val) => {
     if(val == max) return false;
-    
+    return true;
 })
+let secmax = maxremovearr[0]
+for(let i = 1; i<arr.length -1;i++){
+    if(arr[i]>secmax){
+        secmax = maxremovearr[i];
+    }
+}
+console.log(secmax)
