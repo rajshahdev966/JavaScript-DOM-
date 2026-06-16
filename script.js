6e80654c6ed0183fef7e078f45bf2ad9255254cf
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-06-16 08:12:06 +0530

    Update project files

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ let prompt = require("prompt-sync")();
 let n = prompt("Enter the number");
 
 for (let i = 1; i <= n; i++) {
-  for (let j = n - i + 1; j >= 1; j--) {
-    process.stdout.write("* ");
+  for(let j = 1; j<= n - i; j++){
+    process.stdout.write("  ")
   }
-  console.log();
+  for(let k = 1; k<= i;k++){
+    process.stdout.write("* ")
+  }
+  console.log()
 }
