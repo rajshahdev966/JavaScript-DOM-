4fd84ef3e2ee3fddae5309f7755db4bfaec7a20b
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-06-16 23:43:40 +0530

    Auto sync changes

--- dsa.js
+++ dsa.js
@@ -1,18 +1,9 @@
-let prompt = require("prompt-sync")();
-let T = Number(prompt("Enter the Target "));
-let arr = [1,2,3,4,5,0];
-let sum;
-let count = 0;
-for(let i = 0; i< arr.length; i++){
-    sum = 0;
-    for(let j = i; j<arr.length; j++){
-        sum = arr[j] + sum;
-        if(sum === T){
-            count++;
-        };
-    };
-};
-console.log(count);
-
-  
-console.log(arr);
+let arr = [10, 20, 69, 5, 12, 68, 48]
+let T = 70
+let index = -1;
+for(let i = 0; i<arr.length; i++){
+    if(arr[i] === T){
+        index = i;
+    }
+}
+index == -1 ? console.log("Not Found") : console.log(index);
