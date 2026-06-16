8e02be1f3b96427474fb9f360e997f288535f172
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-06-16 08:19:47 +0530

    Auto sync changes

--- script.js
+++ script.js
@@ -1,12 +1,2 @@
 let prompt = require("prompt-sync")();
-let n = prompt("Enter the number");
-
-for (let i = 1; i <= n; i++) {
-  for(let j = 1; j<= n - i; j++){
-    process.stdout.write("  ")
-  }
-  for(let k = 1; k<= i;k++){
-    process.stdout.write("* ")
-  }
-  console.log()
-}
+let arr = [ ]
