41f2f48a667be5e4c1a64088c1f3f034137abf93
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-06-16 09:10:18 +0530

    Update project files

--- script.js
+++ script.js
@@ -1,7 +1,2 @@
-let arr = [65, 25, 5, 6, 55, 56, 75, 87];
-for (let i = 0; i < Math.floor(arr.length / 2); i++) {
-  let temp = arr[i];
-  arr[i] = arr[arr.length - i - 1];
-  arr[arr.length - i - 1] = temp;
-}
-console.log(arr);
+let arr = [1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1];
+
