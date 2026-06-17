0e4a38becae38e0dae47442f75949b3e8862cfe8
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-06-17 07:53:59 +0530

    Updated portfolio content

--- dsa.js
+++ dsa.js
@@ -1,6 +1,17 @@
 let arr = [10, 20, 69, 5, 12, 68, 48];
-for(let i = 0; i< arr.length; i++){
-    for(let j = i; j<arr.length; j++){
-        console.log(arr.slice(i, j + 1));
+function printAllSubarr(array) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i; j < arr.length; j++) {
+      console.log(arr.slice(i, j + 1));
     }
+  }
+}
+
+function countTotalSubarr(array) {
+    let n = array.length;
+    console.log((n * (n+1))/2)
+}
+countTotalSubarr(arr)
+function subarrofKLength(array, k) {
+    for(let i = 0; i<)
 }
