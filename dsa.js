7c1edc7dfe88e149fe1e462ae99a4bb256da9b71
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-06-17 07:49:11 +0530

    Auto sync changes

--- dsa.js
+++ dsa.js
@@ -1,5 +1,5 @@
 let arr = [10, 20, 69, 5, 12, 68, 48];
-for(let i = 0; i< arr.lenght; i++){
+for(let i = 0; i< arr.length; i++){
     for(let j = i; j<arr.length; j++){
         console.log(arr.slice(i, j + 1));
     }
