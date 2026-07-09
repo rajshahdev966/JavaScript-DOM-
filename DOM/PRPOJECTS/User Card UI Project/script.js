f2172f0ece31b2cb5e77d9c136ac405238f2bd23
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-07-09 20:45:58 +0530

    Update project files

--- DOM/PRPOJECTS/User Card UI Project/script.js
+++ DOM/PRPOJECTS/User Card UI Project/script.js
@@ -1,3 +1,7 @@
+
+
+
+const form = document.querySelector("form")
 const nameForm = document.querySelector("#name-form")
 const mailForm = document.querySelector("#mail-form")
 const imageForm = document.querySelector("#image-url")
@@ -7,7 +11,28 @@ const userCollection = document.querySelector(".user-collection")
 let userData = []
 
 
-submitButton.addEventListener("click", ()=>{
+submitButton.addEventListener("click", (events)=>{
+    events.preventDefault();
+    let userName = nameForm.value;
+    let email = mailForm.value
+    let imgUrl = imageForm.value;
+
+    userCollection.innerHTML += `<div class="user-card">
+                <div class="user-card-img">
+                    <img src="${imgUrl}" alt="">
+                </div>
+                <div class="user-card-text">
+                    <div>
+                        <span>${userName}</span>
+                        <p>Email: ${userMail}</p>
+                    </div>
+                    <div class="user-card-buttons">
+                        <button class="edit" onclick="">Edit</button>
+                        <button class="del" onclick="" style="background-color: #e03131; color: white">Delete</button>
+                    </div>
+                </div>
+            </div>`
     
+    form.reset();
 })
 
