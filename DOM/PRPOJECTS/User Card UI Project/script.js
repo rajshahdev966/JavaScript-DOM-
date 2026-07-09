1c4341b322c426597d1784ed3e3bb00864cfd08b
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-07-09 20:58:40 +0530

    Update project files

--- DOM/PRPOJECTS/User Card UI Project/script.js
+++ DOM/PRPOJECTS/User Card UI Project/script.js
@@ -53,29 +53,26 @@ let userData = [
   }
 ]
 
-// userData.forEach((userObj, index)=>{
-//     let userName = userObj.name;
-//     let email = userObj.mail
-//     let imgUrl = userObj.image;
-
-//     if(userName.trim() === "" || email.trim() === "" || imgUrl.trim() === "") return;
-
-//     userCollection.innerHTML += `<div class="user-card">
-//                 <div class="user-card-img">
-//                     <img src="${imgUrl}" alt="">
-//                 </div>
-//                 <div class="user-card-text">
-//                     <div>
-//                         <span>${userName}</span>
-//                         <p>Email: ${email}</p>
-//                     </div>
-//                     <div class="user-card-buttons">
-//                         <button class="edit" onclick="">Edit</button>
-//                         <button class="del" onclick="" style="background-color: #e03131; color: white">Delete</button>
-//                     </div>
-//                 </div>
-//             </div>`
-// })
+userData.forEach((userObj, index)=>{
+    let userName = userObj.name;
+    let email = userObj.mail
+    let imgUrl = userObj.image;
+    userCollection.innerHTML += `<div class="user-card">
+                <div class="user-card-img">
+                    <img src="${imgUrl}" alt="">
+                </div>
+                <div class="user-card-text">
+                    <div>
+                        <span>${userName}</span>
+                        <p>Email: ${email}</p>
+                    </div>
+                    <div class="user-card-buttons">
+                        <button class="edit" onclick="">Edit</button>
+                        <button class="del" onclick="" style="background-color: #e03131; color: white">Delete</button>
+                    </div>
+                </div>
+            </div>`
+})
 
 submitButton.addEventListener("click", (events)=>{
     events.preventDefault();
