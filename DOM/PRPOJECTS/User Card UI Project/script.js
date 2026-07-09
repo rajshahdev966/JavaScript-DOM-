fe47ec3563d842b3f58c94bdea05f3562e1f2740
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-07-09 21:06:17 +0530

    Updated portfolio content

--- DOM/PRPOJECTS/User Card UI Project/script.js
+++ DOM/PRPOJECTS/User Card UI Project/script.js
@@ -1,61 +1,59 @@
-
-
-
-const form = document.querySelector("form")
-const nameForm = document.querySelector("#name-form")
-const mailForm = document.querySelector("#mail-form")
-const imageForm = document.querySelector("#image-url")
-const submitButton = document.querySelector("#submit-but")
-const userCollection = document.querySelector(".user-collection")
+const form = document.querySelector("form");
+const nameForm = document.querySelector("#name-form");
+const mailForm = document.querySelector("#mail-form");
+const imageForm = document.querySelector("#image-url");
+const submitButton = document.querySelector("#submit-but");
+const userCollection = document.querySelector(".user-collection");
 
 let userData = [
   {
-    "id": 1,
-    "name": "Aarav Sharma",
-    "mail": "aarav.sharma@example.com",
-    "image": "https://i.pravatar.cc/150?img=1"
+    id: 1,
+    name: "Aarav Sharma",
+    mail: "aarav.sharma@example.com",
+    image: "https://i.pravatar.cc/150?img=1",
   },
   {
-    "id": 2,
-    "name": "Emma Wilson",
-    "mail": "emma.wilson@example.com",
-    "image": "https://i.pravatar.cc/150?img=2"
+    id: 2,
+    name: "Emma Wilson",
+    mail: "emma.wilson@example.com",
+    image: "https://i.pravatar.cc/150?img=2",
   },
   {
-    "id": 3,
-    "name": "Liam Carter",
-    "mail": "liam.carter@example.com",
-    "image": "https://i.pravatar.cc/150?img=3"
+    id: 3,
+    name: "Liam Carter",
+    mail: "liam.carter@example.com",
+    image: "https://i.pravatar.cc/150?img=3",
   },
   {
-    "id": 4,
-    "name": "Priya Patel",
-    "mail": "priya.patel@example.com",
-    "image": "https://i.pravatar.cc/150?img=4"
+    id: 4,
+    name: "Priya Patel",
+    mail: "priya.patel@example.com",
+    image: "https://i.pravatar.cc/150?img=4",
   },
   {
-    "id": 5,
-    "name": "Noah Brown",
-    "mail": "noah.brown@example.com",
-    "image": "https://i.pravatar.cc/150?img=5"
+    id: 5,
+    name: "Noah Brown",
+    mail: "noah.brown@example.com",
+    image: "https://i.pravatar.cc/150?img=5",
   },
   {
-    "id": 6,
-    "name": "Sophia Davis",
-    "mail": "sophia.davis@example.com",
-    "image": "https://i.pravatar.cc/150?img=6"
+    id: 6,
+    name: "Sophia Davis",
+    mail: "sophia.davis@example.com",
+    image: "https://i.pravatar.cc/150?img=6",
   },
   {
-    "id": 7,
-    "name": "Kabir Mehta",
-    "mail": "kabir.mehta@example.com",
-    "image": "https://i.pravatar.cc/150?img=7"
-  }
-]
+    id: 7,
+    name: "Kabir Mehta",
+    mail: "kabir.mehta@example.com",
+    image: "https://i.pravatar.cc/150?img=7",
+  },
+];
 
-userData.forEach((userObj, index)=>{
+let userCardPrint = () => {
+  userData.forEach((userObj, index) => {
     let userName = userObj.name;
-    let email = userObj.mail
+    let email = userObj.mail;
     let imgUrl = userObj.image;
     userCollection.innerHTML += `<div class="user-card">
                 <div class="user-card-img">
@@ -71,33 +69,24 @@ userData.forEach((userObj, index)=>{
                         <button class="del" onclick="" style="background-color: #e03131; color: white">Delete</button>
                     </div>
                 </div>
-            </div>`
-})
+            </div>`;
+  });
+};
 
-submitButton.addEventListener("click", (events)=>{
-    events.preventDefault();
-    let userName = nameForm.value;
-    let email = mailForm.value
-    let imgUrl = imageForm.value;
+submitButton.addEventListener("click", (events) => {
+  events.preventDefault();
+  let userName = nameForm.value;
+  let email = mailForm.value;
+  let imgUrl = imageForm.value;
 
-    if(userName.trim() === "" || email.trim() === "" || imgUrl.trim() === "") return;
-
-    userCollection.innerHTML += `<div class="user-card">
-                <div class="user-card-img">
-                    <img src="${imgUrl}" alt="">
-                </div>
-                <div class="user-card-text">
-                    <div>
-                        <span>${userName}</span>
-                        <p>Email: ${email}</p>
-                    </div>
-                    <div class="user-card-buttons">
-                        <button class="edit" onclick="">Edit</button>
-                        <button class="del" onclick="" style="background-color: #e03131; color: white">Delete</button>
-                    </div>
-                </div>
-            </div>`
-    
-    form.reset();
-})
-
+  if (userName.trim() === "" || email.trim() === "" || imgUrl.trim() === "")
+    return;
+  let newUser = {
+    name: userName,
+    mail: email,
+    image: imgUrl,
+  }
+  userData.push(newUser);
+  userCardPrint
+  form.reset();
+});
