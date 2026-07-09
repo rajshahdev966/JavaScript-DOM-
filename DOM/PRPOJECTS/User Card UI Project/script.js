fb97021c7e0666cbb00664834f884d0bb5c1d9e8
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-07-09 20:36:57 +0530

    Auto sync changes

--- DOM/PRPOJECTS/User Card UI Project/script.js
+++ DOM/PRPOJECTS/User Card UI Project/script.js
@@ -1,2 +1,13 @@
 const nameForm = document.querySelector("#name-form")
-const mailForm = document.querySelector("#")
+const mailForm = document.querySelector("#mail-form")
+const imageForm = document.querySelector("#image-url")
+const submitButton = document.querySelector("#submit-but")
+const userCollection = document.querySelector(".user-collection")
+
+let userData = []
+
+
+submitButton.addEventListener("click", ()=>{
+    
+})
+
