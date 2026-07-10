f69d389143f48909d724ee59e41f661176416044
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-07-10 10:28:10 +0530

    Update project files

--- DOM/PRPOJECTS/Random Box Position/script.js
+++ DOM/PRPOJECTS/Random Box Position/script.js
@@ -5,7 +5,7 @@ const timeCounter = document.querySelector("span");
 startBut.addEventListener("click", () => {
   let time = 0;
 
-  setInterval(() => {
+  let timer = setInterval(() => {
     time += 1;
     timeCounter.innerHTML = time
     let distTop = `${Math.floor(Math.random() * 99) + 1}%`;
@@ -16,6 +16,6 @@ startBut.addEventListener("click", () => {
   }, 1000);
 
   setTimeout(()=>{
-
+    clearInterval(timer);
   }, 10000)
 });
