3838d0e185795bbc68d95632e02aeecd94204d34
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-07-10 10:25:16 +0530

    Auto sync changes

--- DOM/PRPOJECTS/Random Box Position/script.js
+++ DOM/PRPOJECTS/Random Box Position/script.js
@@ -1,19 +1,21 @@
-const randomBox = document.querySelector(".random-box")
-const startBut = document.querySelector("button")
+const randomBox = document.querySelector(".random-box");
+const startBut = document.querySelector("button");
+const timeCounter = document.querySelector("span");
 
+startBut.addEventListener("click", () => {
+  let time = 0;
 
- 
+  setInterval(() => {
+    time += 1;
+    timeCounter.innerHTML = time
+    let distTop = `${Math.floor(Math.random() * 99) + 1}%`;
+    let distLeft = `${Math.floor(Math.random() * 99) + 1}%`;
 
+  randomBox.style.top = distTop;
+  randomBox.style.left = distLeft;
+  }, 1000);
 
-startBut.addEventListener('click', ()=>{
-    console.log("Hello");
-    let distTop = `${Math.floor(Math.random() * 99) + 1}%`;  
-    let distLeft = `${Math.floor(Math.random() * 99) + 1}%`; 
-    randomBox.style.top = distTop
-    randomBox.style.left = distLeft
+  setTimeout(()=>{
 
-
-})
-
-
-
+  }, 10000)
+});
