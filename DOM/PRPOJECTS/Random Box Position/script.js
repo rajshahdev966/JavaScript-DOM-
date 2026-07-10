95c7f7ead6d8ab9c63e7c1d4221a73677fbae0cd
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-07-10 10:07:15 +0530

    Modified website assets

--- DOM/PRPOJECTS/Random Box Position/script.js
+++ DOM/PRPOJECTS/Random Box Position/script.js
@@ -9,8 +9,8 @@ startBut.addEventListener('click', ()=>{
     console.log("Hello");
     let distTop = `${Math.floor(Math.random() * 99) + 1}%`;  
     let distLeft = `${Math.floor(Math.random() * 99) + 1}%`; 
-    randomBox.setAttribute("top", distTop)
-    randomBox.setAttribute("left", distLeft)
+    randomBox.style.top = distTop
+    randomBox.style.left = distLeft
 
 
 })
