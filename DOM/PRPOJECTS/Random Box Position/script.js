78ccb271ddcc6b379f8b6f5631912fc0a552b62e
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-07-10 21:24:24 +0530

    Modified website assets

--- DOM/PRPOJECTS/Random Box Position/script.js
+++ DOM/PRPOJECTS/Random Box Position/script.js
@@ -1,1 +1,23 @@
-const 
+const randomBox = document.querySelector(".random-box")
+const startButton = document.querySelector("button")
+const timeCount = document.querySelector("#time-count")
+const scoreCount = document.querySelector("#score-count")
+
+let randomPosGen = ()=>{
+    let posTop = (Math.random() * 99) + 1;
+    let posLeft = (Math.random() * 99) + 1;
+    randomBox.style.top = `${posTop}%`
+    randomBox.style.left = `${posLeft}%`
+}
+
+startButton.addEventListener('click', ()=>{
+    let time = 0
+
+    let interval = setInterval(()=>{
+        time += 1;
+        timeCount.innerHTML = time;
+        randomPosGen();
+
+
+    },1000)
+})
