bbb6ae2aec44ec5b405179b436cc22a5894f0282
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-07-10 23:18:53 +0530

    Auto sync changes

--- DOM/PRPOJECTS/Random Box Position/script.js
+++ DOM/PRPOJECTS/Random Box Position/script.js
@@ -1,41 +1,36 @@
-const randomBox = document.querySelector(".random-box")
-const box = document.querySelector(".box")
-const startButton = document.querySelector("button")
-const timeCount = document.querySelector("#time-count")
-const scoreCount = document.querySelector("#score-count")
+const randomBox = document.querySelector(".random-box");
+const box = document.querySelector(".box");
+const startButton = document.querySelector("button");
+const timeCount = document.querySelector("#time-count");
+const scoreCount = document.querySelector("#score-count");
 let interval;
 let time;
-const bigBoxHeight = box.clientHeight
-const ranBoxHeight = randomBox.offsetHeight 
-const bigBoxWidth = box.clientWidth
-const ranBoxWidth = randomBox.offsetWidth 
+const reqHeight = box.clientHeight - randomBox.clientHeight - 1;
+const reqWidth = box.clientWidth - randomBox.clientHeight - 1;
 
+let randomPosGen = () => {
+  time += 1;
+  timeCount.innerHTML = time;
+  let posTop = Math.floor(Math.random() * reqHeight + 1);
+  let posLeft = Math.floor(Math.random() * reqWidth + 1);
+  let red = Math.floor(Math.random() * 255);
+  let blue = Math.floor(Math.random() * 255);
+  let green = Math.floor(Math.random() * 255);
+  randomBox.style.top = `${posTop}%`;
+  randomBox.style.left = `${posLeft}%`;
+  randomBox.style.backgroundColor = `rgb(${red}, ${blue}, ${green})`;
+};
 
-
-let randomPosGen = ()=>{
-    time += 1;
-    timeCount.innerHTML = time;
-    let posTop = Math.floor((Math.random() * 99) + 1);
-    let posLeft = Math.floor((Math.random() * 99) + 1);
-    let red = Math.floor(Math.random() * 255)
-    let blue = Math.floor(Math.random() * 255)
-    let green = Math.floor(Math.random() * 255)
-    randomBox.style.top = `${posTop}%`
-    randomBox.style.left = `${posLeft}%`
-    randomBox.style.backgroundColor = `rgb(${red}, ${blue}, ${green})`
-}
-
-startButton.addEventListener('click', ()=>{
-    time = 0
-    timeCount.innerHTML = time;
-    startButton.disabled = true;
+startButton.addEventListener("click", () => {
+  time = 0;
+  timeCount.innerHTML = time;
+  startButton.disabled = true;
+  clearInterval(interval);
+  interval = setInterval(() => {
+    randomPosGen();
+  }, 1000);
+  setTimeout(() => {
     clearInterval(interval);
-    interval = setInterval(()=>{
-        randomPosGen();
-    },1000)
-    setTimeout(()=>{
-        clearInterval(interval);
-        startButton.disabled = false;
-    },10000)
-})
-
+    startButton.disabled = false;
+  }, 10000);
+});
